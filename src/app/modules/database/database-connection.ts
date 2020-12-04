1819335bccadd74dfb74b7827215e5e83bf16d18
A-frontman <a.frontman.blog@gmail.com>
2020-12-04 14:47:52 +0100

    2 files modified

--- src/app/modules/database/database-connection.ts
+++ src/app/modules/database/database-connection.ts
@@ -1,19 +1,18 @@
-import { ITransactionEntity } from './i-transaction-entity';
-import * as transactionsTable from './mock/#start_angular_database_mock.json';
+import { ITransactionEntity } from "./i-transaction-entity";
+import * as transactionsTable from "./mock/#start_angular_database_mock.json";
 
 export interface IDatabaseConnetion {
-    get(): ITransactionEntity[];
-    create(transaction: ITransactionEntity): boolean;
+  get(): ITransactionEntity[];
+  create(transaction: ITransactionEntity): boolean;
 }
 
-export class DatabaseConnetion implements IDatabaseConnetion{
-    public get(): ITransactionEntity[] {
-        return transactionsTable.data;
-    }
+export class DatabaseConnetion implements IDatabaseConnetion {
+  public get(): ITransactionEntity[] {
+    return transactionsTable.data;
+  }
 
-    public create(transaction: ITransactionEntity): boolean {
-        // Add Transaction to transactionsTable
-        return true;
-    }
-
+  public create(transaction: ITransactionEntity): boolean {
+    // Add Transaction to transactionsTable
+    return true;
+  }
 }
