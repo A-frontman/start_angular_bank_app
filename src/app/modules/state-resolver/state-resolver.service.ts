64c59776a648e56f3f3d752b2100f947a9ed08fc
frontman <a.frontman.blog@gmail.com>
2020-12-11 04:41:17 +0100

    transaction added subject created

--- src/app/modules/state-resolver/state-resolver.service.ts
+++ src/app/modules/state-resolver/state-resolver.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Transaction } from 'src/app/model/transaction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateResolverService {
-
-  constructor() { }
+  public readonly stateUpdated$ = new Subject<Transaction>();
 }
