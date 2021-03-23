df3f3b3102fd85e4e3a33b377ac70e870db2e020
frontman <a.frontman.blog@gmail.com>
2021-03-23 18:08:44 +0100

    space remove

--- src/app/modules/show-transactions/list/transaction-list.component.spec.ts
+++ src/app/modules/show-transactions/list/transaction-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TransactionListComponent } from './transaction-list.component';
 
 describe('TransactionListComponent', () => {
