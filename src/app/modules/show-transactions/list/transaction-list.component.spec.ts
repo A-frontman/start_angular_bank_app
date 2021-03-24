71d46f146e0a8549db2710b37f061b1f35d4f5fb
frontman <a.frontman.blog@gmail.com>
2021-03-24 16:55:00 +0100

    up

--- src/app/modules/show-transactions/list/transaction-list.component.spec.ts
+++ src/app/modules/show-transactions/list/transaction-list.component.spec.ts
@@ -7,7 +7,6 @@ class TransactionRepositoryServiceMock {
 }
 
 fdescribe('TransactionListComponent', () => {
-
   let component: TransactionListComponent;
   let fixture: ComponentFixture<TransactionListComponent>;
 
